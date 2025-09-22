e326ff29db3e3201ca31651363e1813528a181a0
Fatima Faizan <ffarooqi.bese25seecs@seecs.edu.pk>
2025-09-22 21:56:04 +0500

    Add files via upload

---
+++ Fatima/Task1.py
@@ -0,0 +1,12 @@
+name= "fatima45"
+age= 18
+height= 5.4
+is_student= True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+ 
+
---
+++ Fatima/Task2.py
@@ -0,0 +1,12 @@
+a= 2
+b= 6
+    
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+c= float(a/b)
+print("Division: ", c)
+d= int(a//b)
+print("Floor division: ", d)
+print("Modulus: ", a%b)
+print("Power: ", a**b)
