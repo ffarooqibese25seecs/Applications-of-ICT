30c6bc5b77056d55d90b18cd2fee63dd272c7119
manal-faisal12 <manalfaisal48@gmail.com>
2025-09-30 16:08:13 +0500

    Added manal's folder with tasks

--- manal/task1.py
+++
@@ -1,8 +0,0 @@
-name = "Manal" 
-print(name)
-age = 18
-print(age)
-height = 5.6
-print(height) 
-is_Student = True 
-print(is_Student)
--- manal/task2.py
+++
@@ -1,9 +0,0 @@
-a=5
-b=7
-print("the sum of 2 numbers is ", a+b)
-print("subtraction of 2 number is ", a-b)
-print("multiplication of 2 number is ", a*b)
-print("modulus of 2 number is ", a%b)
-print("division of 2 number is ", a/b)
-print("exponential of 2 number is ", a**b)
-print("integer division of 2 number is ", a//b)
